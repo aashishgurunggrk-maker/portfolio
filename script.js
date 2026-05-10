5c5aa1d0ec2cc44278e07e414d13eb62015aea1a
aashishgurunggrk-maker <aasishgurung.grk@gmail.com>
2026-05-10 12:15:18 +0545

    new files mine

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('btn')
+let user = document.querySelector('#user-image')
+
+button.addEventListener('click', () => {
+    user.style.borderRadius = "0%"
+    setTimeout(revertImage, 3000)
+})
+
+
+
+function revertImage() {
+    user.style.borderRadius = "50%"
+}
